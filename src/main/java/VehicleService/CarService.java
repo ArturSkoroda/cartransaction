cd262226cec6b45a9db0eeca18823b5671f25cb6
Artur <artur.skoroda5@gmail.com>
2021-11-22 20:06:25 +0100

    Adding WebConfig and CarService

--- src/main/java/VehicleService/CarService.java
+++ src/main/java/VehicleService/CarService.java
@@ -1,9 +1,11 @@
 package VehicleService;
 
+import CarInformation.Vehicle;
 import Repository.VehicleRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -14,7 +16,21 @@ public class CarService {
     @GetMapping("/")
     public String welcomePage () {return  "Witaj na stronie głównej SKWARKA:-) \n Za kilka chwil będą tu wszystkie auta jakie posiada";}
 
+    @GetMapping("/allVehicleList")
+    public List<Vehicle> getAllVehicleList()
+    {return vehicleRepository.findAll();}
 
-
+    @PostMapping("/addVehicle")
+    public Vehicle addVehicle(Vehicle vehicle){
+        return vehicleRepository.save(vehicle);
+    }
+    @GetMapping("/vehicle/{id}")
+    public Vehicle getVehicleById (@PathVariable(name = "id") Long id) {
+        return vehicleRepository.getById(id);
+    }
+    @DeleteMapping("/vehicle/{id}")
+    public void deleteVehicleById (@PathVariable(name = "id")Long id ){
+        vehicleRepository.deleteById(id);
+    }
 
 }
