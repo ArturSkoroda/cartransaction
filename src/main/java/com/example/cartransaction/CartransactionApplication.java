f20bf3930b29819d9eda1729432c210c979eaa9a
Artur <artur.skoroda5@gmail.com>
2021-12-14 23:40:42 +0100

    adding first example

--- src/main/java/com/example/cartransaction/CartransactionApplication.java
+++ src/main/java/com/example/cartransaction/CartransactionApplication.java
@@ -1,8 +1,10 @@
 package com.example.cartransaction;
 
+import CarInformation.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.time.LocalDateTime;
 
 
 @SpringBootApplication
@@ -10,6 +12,13 @@ public class CartransactionApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CartransactionApplication.class, args);
+
+		Vehicle vehicle= new Vehicle(1L,"Fiat", "126p","jasno-niebieski",
+				FuelType.BENZYNA, TransmissionType.MANUALNA, CarCondition.SPRAWNY, CarStatus.KRAJOWY,
+				100000.07,126,12000,true, LocalDateTime.now(),"foto1","foto2","foto3",
+				"foto","foot5","foto6");
+
+		System.out.println(vehicle);
 	}
 
 }
